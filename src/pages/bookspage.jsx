da24c0c42fa1175abc5696495782fd11d879a15f
Nieem <nieemcse019@gmail.com>
2024-09-23 02:30:43 +0600

    test load json data

--- src/pages/bookspage.jsx
+++ src/pages/bookspage.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
+ import axios from 'axios';
+ import jsondata from '../assets/book-data.json' 
 
 const  Bookspage = () => {
+    const [bookList, setBookList] = useState([]); 
+    const getAllBooks= async ()=>{
+       // const data = await axios.get("../assets/book-data.json"); 
+       const loadData=[...jsondata];
+       console.log(loadData);
+        setBookList(loadData); 
+    }
+    useEffect(() => { 
+        getAllBooks();  
+      }, []); 
+
+
     return (
         <div className='container bg-slate-500 my-28 mx-auto'>
-            <p>hlw</p>
+           { bookList.map((item,i) => ( 
+                        // Presently we only fetch 
+                        // title from the API 
+                        <h4 key={i}>{item.author}</h4> 
+                    )) 
+
+                }
         </div>
     );
 };
